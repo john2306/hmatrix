0c78999b204809138f759bf212f853cf865a71c8
john2306 <johnmendozauni@gmail.com>
2020-03-25 23:41:02 -0500

    Update production.py

--- newspaper/settings/production.py
+++ newspaper/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['192.34.62.40','ianalyticsdata.com/','www.ianalyticsdata.com']
+ALLOWED_HOSTS = ['192.34.62.40','ianalyticsdata.com','www.ianalyticsdata.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
