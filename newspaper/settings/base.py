55cd12ecf54a588f4be26544e804b19a05c6e112
john2306 <johnmendozauni@gmail.com>
2020-06-22 10:22:00 -0500

    Mejoras

--- newspaper/settings/base.py
+++ newspaper/settings/base.py
@@ -120,10 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+
 
 
 AUTH_USER_MODEL = 'users.CustomUser' # new
